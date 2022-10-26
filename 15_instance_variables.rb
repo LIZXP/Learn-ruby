fa2ac3bfa01be2e398753b47ecf3cf4e5242f069
LIZXP <Peter.xli@outlook.com>
2022-10-25 19:32:31 -0600

    ruby setter and getter method

--- 15_instance_variables.rb
+++ 15_instance_variables.rb
@@ -1,12 +1,21 @@
 class Person
-  attr_reader :name
+  #attr_reader :name
+  #attr_writer :name
+  attr_accessor :name
   def initialize(name)
     @name = name
   end
-  #def name
-  #  @name
-  #end
+  # this is the manual way to defind  
+  def name
+    @name
+  end
+  # this is the manual way to defind attr_writer
+  #def name=(name)
+  #  @name = name
+  #end  
 end
 
 p = Person.new('L. Ron')
+puts p.name
+p.name = "New Name"
 puts p.name
